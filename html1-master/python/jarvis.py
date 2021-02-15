f7ed89a4c930ccbe221695ced592dc3ecf96935a
saveer0510 <67908511+saveer0510@users.noreply.github.com>
2021-02-15 14:53:42 +0530

    Delete html1-master directory

--- html1-master/python/jarvis.py
+++
@@ -1,9 +0,0 @@
-
-import pyttsx3
-import speech_recogition as sr
-engine=pyttsx3.init('sapi5')
-
-
-voices =engine.getProperty('voices')
-
-print(voices.id)
--- html1-master/s.js
+++
@@ -1,7 +0,0 @@
-
-function myFunction(){
-var url=window.location.href;
-const uri=new URL(url);
-
-document.getElementById("name").innerHTML=uri.searchParams.get("user")
-}
